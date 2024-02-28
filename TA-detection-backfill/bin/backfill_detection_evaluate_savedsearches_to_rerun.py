aeeae587fe657e8bc30a33775d09736d17faeebc
LmR <letmer00t@gmail.com>
2024-02-28 19:07:43 +0100

    feat: support relative times when building detection backfills

--- TA-detection-backfill/bin/backfill_detection_evaluate_savedsearches_to_rerun.py
+++ TA-detection-backfill/bin/backfill_detection_evaluate_savedsearches_to_rerun.py
@@ -7,7 +7,7 @@ import splunk.Intersplunk
 import re
 import sys
 import globals
-from common import Settings, LoggerFile
+from common import Settings, LoggerFile, RelativeTime
 from ta_logging import setup_logging
 import splunklib.client as client
 import datetime
@@ -42,13 +42,14 @@ if __name__ == '__main__':
 
     # Get settings
     configuration = Settings(spl, settings, logger)
+    now = datetime.datetime.now()
 
     # Process each result
     for result in results:
         
         # Extract useful information
-        outage_period_earliest = result["outage_period_earliest"]
-        outage_period_latest = result["outage_period_latest"]
+        outage_period_earliest = RelativeTime(pattern=result["outage_period_earliest"], time=now.timestamp(), logger=logger).datetime_calculated.timestamp()
+        outage_period_latest = RelativeTime(pattern=result["outage_period_latest"], time=now.timestamp(), logger=logger).datetime_calculated.timestamp()
         savedsearches_regex = result["savedsearches_regex"]
 
         logger_file.info("008","'{0}' ran the script with the following inputs: outage_period_earliest='{1}', outage_period_latest='{2}', savedsearches_regex='{3}'".format(settings["owner"],outage_period_earliest,outage_period_latest,savedsearches_regex))
@@ -95,46 +96,40 @@ if __name__ == '__main__':
             logger_file.debug("021","Got dispatch_earliest='{0}', dispatch_latest='{1}, cron_schedule='{2}'".format(ss_dispatch_earliest,ss_dispatch_latest,savedsearch['content']['cron_schedule']))
         
             # Phase 1: Identify earliest outage time to first latest time period
-            if filter_timestamp.search(outage_period_earliest):
-                # It's a timestamp, convert it to float
-                outage_period_earliest_f = float(outage_period_earliest)
-                outage_period_latest_f = float(outage_period_latest)
-                next_schedule_f = outage_period_earliest_f + ss_cron.next(now=datetime.datetime.fromtimestamp(outage_period_earliest_f,tzlocal.get_localzone()),default_utc=False)
-                
-                next_dispatch_earliest_f = next_schedule_f
-                if ss_dispatch_earliest == "0" and ss_dispatch_latest is None:
-                        backfill_output["dispatch_time"] = -1
-                        logger_file.warn("023","Savedsearch '{0}' is looking on 'All time'. To avoid performance issues, this savedsearch will be skipped.".format(savedsearch.name))   
-                elif filter_timestamp.search(ss_dispatch_earliest) and filter_timestamp.search(ss_dispatch_latest):
-                    # Run the savedsearch as is only once if it's in the range of outage
-                    if (float(ss_dispatch_latest) > outage_period_earliest_f and outage_period_latest_f > float(ss_dispatch_latest)) or (float(ss_dispatch_earliest) > outage_period_earliest_f and outage_period_latest_f > float(ss_dispatch_earliest)) or (float(ss_dispatch_earliest) > outage_period_earliest_f and outage_period_latest_f > float(ss_dispatch_latest)):
-                        backfill_output["comment"] = ["Phase 1: Evaluate first outage range","Time: Next dispatch time calculated from the cron, #0 (static savedsearch, so only once)"]
-                        backfill_output["dispatch_time"] = next_schedule_f
-                    else:
-                        backfill_output["dispatch_time"] = -1
-                        logger_file.warn("024","Static times from the savedsearch '{0}' aren't contained in the outage period '{1}->{2}', this savedsearch will be skipped.".format(savedsearch.name,str(outage_period_earliest_f),str(outage_period_latest_f)))
-                elif filter_timestamp.search(ss_dispatch_earliest):
-                    # Earliest is a static timestamp
-                    ss_dispatch_earliest_f = float(ss_dispatch_earliest)
-                    if datetime.datetime.fromtimestamp(ss_dispatch_earliest_f,tzlocal.get_localzone()) > datetime.datetime.fromtimestamp(outage_period_latest_f,tzlocal.get_localzone()):
-                        # Case 1: static timestamp is earlier than the end of the outage, we skip it
-                        backfill_output["dispatch_time"] = -1
-                        logger_file.warn("025","Static earliest time from the savedsearch '{0}' is later than the end of the outage '{1}', this range will be skipped.".format(str(ss_dispatch_earliest),str(outage_period_latest_f)))
-                    else:
-                        # Case 2: static timestamp is later than or equals to the end of the outage, we take it
-                        backfill_output["dispatch_time"] = next_schedule_f
-                        backfill_output["comment"] = ["Phase 1: Evaluate first outage range","Time: Static earliest time is earlier than next schedule, keep next schedule time"]
-                elif filter_timestamp.search(ss_dispatch_latest):
-                    # Latest is a static timestamp
+            outage_period_earliest_f = float(outage_period_earliest)
+            outage_period_latest_f = float(outage_period_latest)
+            next_schedule_f = outage_period_earliest_f + ss_cron.next(now=datetime.datetime.fromtimestamp(outage_period_earliest_f,tzlocal.get_localzone()),default_utc=False)
+            
+            next_dispatch_earliest_f = next_schedule_f
+            if ss_dispatch_earliest == "0" and ss_dispatch_latest is None:
                     backfill_output["dispatch_time"] = -1
-                    logger_file.warn("026","Savedsearch '{0}' has static timestamp as latest but not on earliest. This savedsearch will be skipped as not supported by the script.".format(savedsearch.name))
-                else:
-                    backfill_output["comment"] = ["Phase 1: Evaluate first outage range","Time: Next dispatch time calculated from the cron, #0"]
+                    logger_file.warn("023","Savedsearch '{0}' is looking on 'All time'. To avoid performance issues, this savedsearch will be skipped.".format(savedsearch.name))   
+            elif filter_timestamp.search(ss_dispatch_earliest) and filter_timestamp.search(ss_dispatch_latest):
+                # Run the savedsearch as is only once if it's in the range of outage
+                if (float(ss_dispatch_latest) > outage_period_earliest_f and outage_period_latest_f > float(ss_dispatch_latest)) or (float(ss_dispatch_earliest) > outage_period_earliest_f and outage_period_latest_f > float(ss_dispatch_earliest)) or (float(ss_dispatch_earliest) > outage_period_earliest_f and outage_period_latest_f > float(ss_dispatch_latest)):
+                    backfill_output["comment"] = ["Phase 1: Evaluate first outage range","Time: Next dispatch time calculated from the cron, #0 (static savedsearch, so only once)"]
                     backfill_output["dispatch_time"] = next_schedule_f
+                else:
+                    backfill_output["dispatch_time"] = -1
+                    logger_file.warn("024","Static times from the savedsearch '{0}' aren't contained in the outage period '{1}->{2}', this savedsearch will be skipped.".format(savedsearch.name,str(outage_period_earliest_f),str(outage_period_latest_f)))
+            elif filter_timestamp.search(ss_dispatch_earliest):
+                # Earliest is a static timestamp
+                ss_dispatch_earliest_f = float(ss_dispatch_earliest)
+                if datetime.datetime.fromtimestamp(ss_dispatch_earliest_f,tzlocal.get_localzone()) > datetime.datetime.fromtimestamp(outage_period_latest_f,tzlocal.get_localzone()):
+                    # Case 1: static timestamp is earlier than the end of the outage, we skip it
+                    backfill_output["dispatch_time"] = -1
+                    logger_file.warn("025","Static earliest time from the savedsearch '{0}' is later than the end of the outage '{1}', this range will be skipped.".format(str(ss_dispatch_earliest),str(outage_period_latest_f)))
+                else:
+                    # Case 2: static timestamp is later than or equals to the end of the outage, we take it
+                    backfill_output["dispatch_time"] = next_schedule_f
+                    backfill_output["comment"] = ["Phase 1: Evaluate first outage range","Time: Static earliest time is earlier than next schedule, keep next schedule time"]
+            elif filter_timestamp.search(ss_dispatch_latest):
+                # Latest is a static timestamp
+                backfill_output["dispatch_time"] = -1
+                logger_file.warn("026","Savedsearch '{0}' has static timestamp as latest but not on earliest. This savedsearch will be skipped as not supported by the script.".format(savedsearch.name))
             else:
-                # outage_period_earliest can't be a pattern, exit
-                logger_file.error("060","Outage earliest period must be a timestamp, got '{0}'".format(outage_period_earliest))
-                sys.exit(60)
+                backfill_output["comment"] = ["Phase 1: Evaluate first outage range","Time: Next dispatch time calculated from the cron, #0"]
+                backfill_output["dispatch_time"] = next_schedule_f
 
             if backfill_output["dispatch_time"] != -1:
                 # No issue found during the processing, we can consider this
